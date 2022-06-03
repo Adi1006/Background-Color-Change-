04c939d53d5258dd21eed834e44cc0addc1346e6
Adi1006 <adityag1006@gmail.com>
2022-06-03 20:57:14 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const body=document.body;
+const switchBtn=document.querySelector('.switch');
+const switchpara=document.querySelector('.color');
+
+switchBtn.addEventListener('click',function () {
+let color1 =  getRandomNum();
+let color2 =  getRandomNum();
+let color3 =  getRandomNum();
+const colorString=`rgb(${color1},${color2},${color3})`;
+body.style.backgroundColor=colorString;
+switchpara.innerText=colorString;
+});
+
+function getRandomNum() {
+    return Math.floor(Math.random()*256);
+}
